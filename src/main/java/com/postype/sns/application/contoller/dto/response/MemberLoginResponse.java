0bde32262e307f27eb136f95c0c7ab473437a2ed
reneener <jhjin2u@gmail.com>
2023-01-04 22:26:16 +0900

    [#5] 로그인 기능 구현하기

---
+++ src/main/java/com/postype/sns/application/contoller/dto/response/MemberLoginResponse.java
@@ -0,0 +1,12 @@
+package com.postype.sns.application.contoller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberLoginResponse {
+
+	private String token;
+
+}
--- src/main/java/com/postype/sns/application/exception/GlobalControllerAdvice.java
+++ src/main/java/com/postype/sns/application/exception/GlobalControllerAdvice.java
@@ -2,19 +2,27 @@ package com.postype.sns.application.exception;
 
 import com.postype.sns.application.contoller.dto.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
-public class GlobalContollerAdvice {
+public class GlobalControllerAdvice {
 
 	@ExceptionHandler(MemberException.class)
 	public ResponseEntity<?> applicationHandler(MemberException e){
 		log.error("Error occurs {}", e.toString());
 		return ResponseEntity.status(e.getErrorCode().getStatus())
 			.body(Response.error(e.getErrorCode().name()));
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<?> applicationHandler(RuntimeException e){
+		log.error("Error occurs {}", e.toString());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.name()));
 	}
 
 }
